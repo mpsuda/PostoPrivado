230a7655f806be59743196c8f1dc0764bcfee192
Anderson Marcondes Santana Junior <69247704+jujusantana@users.noreply.github.com>
2023-03-26 15:47:17 -0300

    Update Abastecer.java

--- ProdutoServico/Abastecer.java
+++ ProdutoServico/Abastecer.java
@@ -11,13 +11,13 @@ public class Abastecer {
 			
 		if (tipoCombustivel == 1) {
 			quantidadeCombustivel = valorAbastecer / precoGasolina;
-			System.out.printf("Foi abastecido o valor de R$ %.2f, rendendo a quantidade de %.3f litros. ", valorAbastecer, quantidadeCombustivel);
+			System.out.printf("Foi abastecido o valor de R$ %.2f, rendendo a quantidade de %.3f litros.\n ", valorAbastecer, quantidadeCombustivel);
 		} else if (tipoCombustivel == 2) {
 			quantidadeCombustivel = valorAbastecer / precoEtanol;
-			System.out.printf("Foi abastecido o valor de R$ %.2f, rendendo a quantidade de %.3f litros. ", valorAbastecer, quantidadeCombustivel);
+			System.out.printf("Foi abastecido o valor de R$ %.2f, rendendo a quantidade de %.3f litros.\n ", valorAbastecer, quantidadeCombustivel);
 		} else if (tipoCombustivel == 3) {
 			quantidadeCombustivel = valorAbastecer / precoDiesel;
-			System.out.printf("Foi abastecido o valor de R$ %.2f, rendendo a quantidade de %.3f litros. ", valorAbastecer, quantidadeCombustivel);
+			System.out.printf("Foi abastecido o valor de R$ %.2f, rendendo a quantidade de %.3f litros.\n ", valorAbastecer, quantidadeCombustivel);
 		} else {
 			System.out.println("Opção inválida! Por favor, reinicie e escolha um tipo de combustível disponível: Gasolina, Álcool ou Diesel ");
 		}
